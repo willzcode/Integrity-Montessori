42c15ce137ee1404fbfb7ef7fcc9eef230550b67
Atabisa Williams <atabisawilliams@gmail.com>
2024-11-13 08:17:34 -0800

    working on swiper

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-// initializeSwiper.js
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-  
+  slidesPerView: 1,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
