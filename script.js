70e1e4473f01f52bb6ad9987ee0c8e4e9a5111a1
Atabisa Williams <atabisawilliams@gmail.com>
2024-12-13 06:57:31 -0800

    working on header

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const burger = document.querySelector(".burger");
 const navLinks = document.querySelector(".nav-links");
+const navItems = document.querySelectorAll(".nav-links a");
 
 burger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
   burger.classList.toggle("toggle");
 });
+
+// Close menu on link click
+navItems.forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+    burger.classList.remove("toggle");
+  });
+});
+
 
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
